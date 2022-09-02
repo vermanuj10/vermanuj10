99b6afc90e8e50934a01cd3f3ad8ff93f655acd5
verma.anuj@principal.com <verma.anuj@principal.com>
2022-09-02 10:31:03 +0000

    COMM

--- models/customer_orders.sql
+++ models/customer_orders.sql
@@ -7,20 +7,20 @@ with custOrders as (
         max(order_date) as most_recent_order_date,
         count(D) as number_of_orders
         
-    from {{ref('raw_orders')}} group by 1
+    from {{ref('RAW_ORDERS')}} group by 1
 ),
 
 final as (
 
     select
-        {{ref('raw_customers')}}.id as customer_id,
-        {{ref('raw_customers')}}.first_name,
-        {{ref('raw_customers')}}.last_name,
+        {{ref('RAW_CUSTOMERS')}}.id as customer_id,
+        {{ref('RAW_CUSTOMERS')}}.first_name,
+        {{ref('RAW_CUSTOMERS')}}.last_name,
         co.first_order_date,
         co.most_recent_order_date,
         coalesce(co.number_of_orders, 0) as number_of_orders
 
-    from {{ref('raw_customers')}}, custOrders co where customer_id = co.custId
+    from {{ref('RAW_CUSTOMERS')}}, custOrders co where customer_id = co.custId
 
     --left join custOrders using (custId)
 
