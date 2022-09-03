06431222a95fe8b1ceb01572181472af64a054ad
verma.anuj@principal.com <verma.anuj@principal.com>
2022-09-03 11:47:58 +0000

    commit 11

---
+++ models/custOrders/CUSTORDERS.sql
@@ -0,0 +1,33 @@
+{{ config(materialized="view", schema="dbt_averma") }}
+with custOrders as (
+        select
+          
+        cust_id as custId,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+        
+    from {{ref('RAW_ORDERS')}} group by 1
+),
+
+final as (
+    select
+        {{ref('RAW_CUSTOMERS')}}.id as customer_id,
+        {{ref('RAW_CUSTOMERS')}}.first_name,
+        {{ref('RAW_CUSTOMERS')}}.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders
+
+    from {{ref('RAW_CUSTOMERS')}}, CUSTORDERS co where customer_id = co.custId 
+)
+
+    --left join custOrders using (custId)
+
+
+
+ (select * from final)
+
+
+
+
